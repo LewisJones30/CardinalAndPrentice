438dafbd99d5a95d08434997c736b27847db5730
ShemSkillman <43577362+ShemSkillman@users.noreply.github.com>
2020-03-04 15:56:10 +0000

    Animation start
    Completed tasks:
    - Replace cardinal with low poly knight
    - Put animation controller with blend tree and walk-run states
    - Mover script communicates with animator

---
+++ PROC204/Assets/Scripts/Controllers/P1KeyboardController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1KeyboardController : MonoBehaviour
+{
+    Mover mover;
+
+    private void Awake()
+    {
+        mover = GetComponent<Mover>();
+    }
+
+    private void FixedUpdate()
+    {
+        mover.Move(Input.GetAxis("Horizontal"));
+    }
+}
--- PROC204/Assets/Scripts/Movement/Mover.cs
+++ PROC204/Assets/Scripts/Movement/Mover.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,10 +9,12 @@ public class Mover : MonoBehaviour
     [SerializeField] float jumpForce = 20f;
 
     Rigidbody rb;
+    Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     public void Move(float input)
@@ -19,6 +22,15 @@ public class Mover : MonoBehaviour
         float moveForce = input * moveSpeed;
 
         rb.AddForce(new Vector3(moveForce, 0f, 0f), ForceMode.Acceleration);
+
+        UpdateAnimator();
+    }
+
+    private void UpdateAnimator()
+    {
+        //print(rb.velocity);
+        float speed = rb.velocity.x;
+        animator.SetFloat("forwardSpeed", Mathf.Abs(speed));
     }
 
     public void Jump()
