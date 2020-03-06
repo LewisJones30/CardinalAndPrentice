76bc225abe2816d301768b142bc4b537f794b104
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2020-03-06 11:58:35 +0000

    Scripts added

---
+++ PROC204/Assets/Scripts/Menu Navigation/ControllerScreenControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
+public class ControllerScreenControl : MonoBehaviour
+{
+    public UnityEvent southPress, eastPress, northPress, westPress, startPress;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Gamepad.all[0].buttonSouth.isPressed == true)
+        {
+            southPress.Invoke();
+        }
+        else if (Gamepad.all[0].buttonEast.isPressed == true)
+        {
+            eastPress.Invoke();
+        }
+        else if (Gamepad.all[0].buttonNorth.isPressed == true)
+        {
+            northPress.Invoke();
+        }
+        else if (Gamepad.all[0].buttonWest.isPressed == true)
+        {
+            westPress.Invoke();
+        }
+        else if (Gamepad.all[0].startButton.isPressed == true)
+        {
+            startPress.Invoke();
+        }
+    }
+}
---
+++ PROC204/Assets/Scripts/Menu Navigation/SceneTransition.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Level1()
+    {
+
+    }
+    public void Level2()
+    {
+
+    }
+    public void GameOver()
+    {
+    }
+    public void EndScreen()
+    {
+        SceneManager.LoadScene("GameOver"); //Load the GameOver scene.
+    }
+    public void MoveFromEndScreen()
+    {
+        if (PlayerPrefs.GetInt("Level1Status") == 1)
+        {
+            SceneManager.LoadScene("Level1");
+        }
+        else if (PlayerPrefs.GetInt("Level2Status") == 1)
+        {
+            SceneManager.LoadScene("Level2");
+        }
+
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("StartupProject");
+    }
+}
