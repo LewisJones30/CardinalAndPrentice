ef090b62e85b2c873b3308b011c9a493bf919a59
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2020-03-13 12:29:18 +0000

    Added HealthUI script, modified health script.
    Added a new HealthUI script shell. Currently requires sprites to be added, and some experimentation.
    Modified the Health script to invoke HealthUI script, if attached to the "Cardinal" gameobject.

--- PROC204/Assets/Scripts/Combat/Health.cs
+++ PROC204/Assets/Scripts/Combat/Health.cs
@@ -1,22 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] int health = 50;
-
+    [SerializeField] int health = 50; //Serialise so you can edit in editor
     Animator animator;
-
+    string objectName;
     bool isDead = false;
+    public HealthUI healthScript; //Used for invoking healthUI update
 
     private void Awake()
     {
+
         animator = GetComponent<Animator>();
+    }
+    private void Start()
+    {
+        objectName = this.gameObject.name;
+
     }
 
     public void DealDamage(int damage)
     {
+        if (objectName == "Cardinal")
+        {
+            healthScript.TakeHealthUpdate(); //Updates the health UI
+        }
+
         if (isDead) return;
 
         health -= damage;
---
+++ PROC204/Assets/Scripts/UIScripts/HealthUI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+
+    public Sprite threeHearts, twoHearts, OneHeart, twoHalfHearts, oneHalfHearts, zeroHalfHearts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void TakeHealthUpdate()
+    {
+        Sprite currentSprite = GetComponent<Sprite>();
+        var currentSpriteName = currentSprite.name;
+        if (currentSpriteName.ToString() == "threeHearts")
+        {
+            //Instantiate 2.5 hearts sprite
+        }
+        else if (currentSpriteName.ToString() == "twoHalfHearts")
+        {
+            //Instantiate 2 hearts sprite
+        }
+        else if (currentSpriteName.ToString() == "twoHearts")
+        {
+            //Instantiate 1.5 hearts sprite
+        }
+        else if (currentSpriteName.ToString() == "oneHalfHearts")
+        {
+            //Instantiate 1 hearts sprite
+        }
+        else if (currentSpriteName.ToString() == "oneHeart")
+        {
+            //Instantiate 0.5 hearts sprite
+        }
+        else
+        {
+            //Default case
+        }
+    }
+}
