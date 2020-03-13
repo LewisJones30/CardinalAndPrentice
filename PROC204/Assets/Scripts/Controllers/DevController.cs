d940aa205927d4438dec62b94c570d35aabc940a
ShemSkillman <43577362+ShemSkillman@users.noreply.github.com>
2020-03-13 14:51:25 +0000

    Smooth camera
    Completed tasks:
    - Created smooth virtual camera that follows Cardinal
    - Dev controls so keyboard can be used to control Cardinal
    - Altered jump force lower

---
+++ PROC204/Assets/Scripts/Controllers/DevController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DevController : MonoBehaviour
+{
+
+    Mover mover;
+    Melee melee;
+
+    void Awake()
+    {
+        mover = GetComponent<Mover>();
+        melee = GetComponent<Melee>();
+    }    
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) mover.Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        mover.Move(Input.GetAxisRaw("Horizontal"));
+
+        if (Input.GetMouseButtonDown(0)) melee.Swing();
+    }
+}
