90f0e1a837ede956920f86b753a0f18c78b31003
ShemSkillman <43577362+ShemSkillman@users.noreply.github.com>
2020-03-14 19:24:38 +0000

    Enemy pursues player
    Completed tasks:
    - Enemies scan for player within a fixed range
    - Enemies pursue player when player in sight range
    - Enemies turn depending on which side player is

--- PROC204/Assets/Scripts/AI/AIController.cs
+++ PROC204/Assets/Scripts/AI/AIController.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class AIController : MonoBehaviour
 {
-    [SerializeField] float patrolSpeedFraction = 0.2f;
+    [SerializeField] float patrolSpeedFraction = 0.6f;
+    [SerializeField] float pursueSpeedFraction = 0.8f;
     [SerializeField] float turnRefreshRate = 1f;
+    [SerializeField] float findPlayerRate = 0.5f;
+    [SerializeField] float lineOfSight = 5f;
+    [SerializeField] LayerMask targetMask;
+
+    Transform currentTarget;
+    float speedFraction = 1f;
 
     Mover mover;
 
@@ -17,23 +24,70 @@ public class AIController : MonoBehaviour
         mover = GetComponent<Mover>();
     }
 
+    private void Start()
+    {
+        StartCoroutine(FindPlayer());
+    }
+
     private void FixedUpdate()
     {
-        mover.Move(moveDirection * patrolSpeedFraction);
+        mover.Move(moveDirection * speedFraction);
+    }
+
+    IEnumerator FindPlayer()
+    {
+        while(true)
+        {
+            if (currentTarget == null) speedFraction = patrolSpeedFraction;
+            else speedFraction = pursueSpeedFraction;
+
+            yield return new WaitForSeconds(findPlayerRate);
+
+            if (currentTarget != null)
+            {
+                float targetDistance = Vector3.Distance(transform.position, currentTarget.position);
+
+                if (targetDistance > lineOfSight) currentTarget = null;
+                else continue;
+            }
+
+            Collider[] targets = Physics.OverlapSphere(transform.position, lineOfSight, targetMask);
+
+            if (targets.Length < 1) continue;
+
+            currentTarget = targets[0].transform;
+        }        
     }
 
     private void Update()
     {
+        if (currentTarget != null)
+        {
+            float targetDirection = Mathf.Sign(currentTarget.position.x - transform.position.x);
+
+            if (targetDirection != moveDirection) Turn();
+        }
+
         if (mover.GetVelocity().magnitude < 1f && canTurn)
         {
-            canTurn = false;
-            moveDirection *= -1;
-            Invoke(nameof(RefreshTurn), turnRefreshRate);
+            Turn();
         }
+    }
+
+    private void Turn()
+    {
+        canTurn = false;
+        moveDirection *= -1;
+        Invoke(nameof(RefreshTurn), turnRefreshRate);
     }
 
     private void RefreshTurn()
     {
         canTurn = true;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, lineOfSight);
     }
 }
