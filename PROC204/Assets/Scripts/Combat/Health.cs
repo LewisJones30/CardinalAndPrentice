dfae66bcf69618d9837c401c0204d614455ff13a
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2020-03-15 19:12:19 +0000

    another small fix

---
+++ PROC204/Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int health = 50;
+
+    Animator animator;
+
+    ParticleSystem sleepParticle;
+
+    bool isDead = false;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        sleepParticle = GetComponent<ParticleSystem>();
+    }
+
+    public void DealDamage(int damage)
+    {
+        if (isDead) return;
+
+        health -= damage;
+
+        if (health < 1) Die();
+    }
+
+    private void Die()
+    {
+        isDead = true;
+
+        gameObject.layer = LayerMask.NameToLayer("Passable");
+        animator.SetTrigger("Die");
+
+        sleepParticle.Play();
+    }
+}
