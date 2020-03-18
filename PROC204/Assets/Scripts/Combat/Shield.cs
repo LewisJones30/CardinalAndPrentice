da876eb1a60165228b5eb2d731e27205fa612061
ShemSkillman <43577362+ShemSkillman@users.noreply.github.com>
2020-03-18 19:32:26 +0000

    Shield bounce
    Completed tasks:
    - Shield applies forward force to enemies making them bounce off

--- PROC204/Assets/Scripts/Combat/Shield.cs
+++ PROC204/Assets/Scripts/Combat/Shield.cs
@@ -4,62 +4,13 @@ using UnityEngine;
 
 public class Shield : MonoBehaviour
 {
-    [SerializeField] GameObject shieldPrefab;
-    [SerializeField] float shieldDistance = 3f;
-    [SerializeField] float shieldDelayAfterWeaponFire = 2f;
+    [SerializeField] float bounciness = 70f;
 
-    GameObject currentShield;
-    Transform cardinal;
-    RangedWeapon rangedWeapon;
-
-    float timeSinceWeaponFire = 0f;
-
-    private void Awake()
+    private void OnCollisionEnter(Collision collision)
     {
-        cardinal = GameObject.FindWithTag("Player 1").transform;
-        rangedWeapon = GetComponent<RangedWeapon>();
-    }
-    private void OnEnable()
-    {
-        rangedWeapon.onFire += ResetTimer;
-    }
+        Rigidbody victim = collision.gameObject.GetComponent<Rigidbody>();
 
-    private void OnDisable()
-    {
-        rangedWeapon.onFire -= ResetTimer;
-    }
-
-    public void Protect(Vector2 dir)
-    {
-        if (dir.Equals(Vector2.zero) || timeSinceWeaponFire < shieldDelayAfterWeaponFire)
-        {
-            if (currentShield != null) Destroy(currentShield);
-            return;
-        }
-
-        if (currentShield == null)
-        {
-            currentShield = Instantiate(shieldPrefab, cardinal);
-        }
-
-        PositionShield(dir);
-    }
-
-    private void PositionShield(Vector2 dir)
-    {
-        Vector3 v3Dir = new Vector3(dir.x, dir.y, 0f);
-        Vector3 shieldPos = cardinal.position + v3Dir * shieldDistance;
-        currentShield.transform.position = shieldPos;
-        currentShield.transform.right = dir;
-    }
-
-    private void ResetTimer()
-    {
-        timeSinceWeaponFire = 0f;
-    }
-
-    private void Update()
-    {
-        timeSinceWeaponFire += Time.deltaTime;
+        if (victim == null) return;
+        victim.AddForce(transform.right * bounciness, ForceMode.Impulse);
     }
 }
