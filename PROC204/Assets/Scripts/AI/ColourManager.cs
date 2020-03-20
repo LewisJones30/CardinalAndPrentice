284858c5e9e56e600a3ac9a320cd106227ff6224
ShemSkillman <43577362+ShemSkillman@users.noreply.github.com>
2020-03-20 11:26:15 +0000

    Enemy variants
    Completed tasks:
    - Imported enemy textures
    - Colours randomly assigned to enemy when game starts
    - More enemy prefabs with different avatars from the medieval pack

---
+++ PROC204/Assets/Scripts/AI/ColourManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourManager : MonoBehaviour
+{
+    [SerializeField] ColourContainer[] entityColours;
+
+    ColourContainer entityColour;
+
+    public ColourValue Colour { get => entityColour.colour; }
+
+    private void Awake()
+    {
+        SetColour();
+
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer renderer in GetComponentsInChildren<Renderer>())
+        {
+            renderer.material = entityColour.colourMaterial;
+        }
+    }
+
+    private void SetColour()
+    {
+        int randomIndex = UnityEngine.Random.Range(0, entityColours.Length);
+        entityColour = entityColours[randomIndex];
+    }
+
+    [System.Serializable]
+    class ColourContainer
+    {
+        public ColourValue colour;
+        public Material colourMaterial;
+    }
+}
+
+public enum ColourValue
+{
+    Yellow,
+    Red,
+    Green,
+    Blue
+}
