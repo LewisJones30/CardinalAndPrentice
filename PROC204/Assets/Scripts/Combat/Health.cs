bb4f7689a1d638f1b2137f0942cff9da0d4e6d6e
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2020-03-25 12:23:40 +0000

    added health script back

---
+++ PROC204/Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int health = 50;
+    [SerializeField] HealthUI healthUI;
+
+    Animator animator;
+    ParticleSystem sleepParticle;
+    ComboSystem CardinalCombo;
+
+    public delegate void OnHealthChange();
+    public event OnHealthChange onHealthChange;
+
+    public bool IsDead { get; private set; } = false;
+    public int HealthPoints { get { return health; } }
+    public ColourValue ColourWeakness { get; set; } = ColourValue.None;
+
+    const int damageMultiplier = 3;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        sleepParticle = GetComponent<ParticleSystem>();
+        if (this.gameObject.name == "Cardinal")
+        {
+            CardinalCombo = GetComponent<ComboSystem>();
+        }
+    }
+
+    public void DealDamage(int damage)
+    {
+        if (IsDead) return;
+
+<<<<<<< HEAD
+        health -= damage;
+        
+        if (gameObject.name == "Cardinal")
+        {
+            healthUI.TakeHealthUpdate();
+=======
+        health -= damage;
+        
+        if (gameObject.name == "Cardinal")
+        {
+            healthUI.TakeHealthUpdate();
+            CardinalCombo.decreaseDamage();
+
+>>>>>>> Combo-System
+        }
+
+        if (health < 1) Die();
+
+        onHealthChange?.Invoke();
+    }
+
+    public void DealDamage(int damage, ColourValue colour)
+    {
+        if (ColourWeakness == colour) damage *= damageMultiplier;
+
+        DealDamage(damage);
+    }
+
+    private void Die()
+    {
+        IsDead = true;
+        if (this.gameObject.name == "Cardinal")
+        {
+            healthUI.gameOver();
+        }
+
+        animator.SetTrigger("Die");
+
+        sleepParticle.Play();
+    }
+
+}
