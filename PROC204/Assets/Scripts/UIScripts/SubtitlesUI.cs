fb7f417d1b95aef4ba5f72e6e2b2f9176cb81b8b
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2020-03-31 15:06:59 +0100

    Added subtitles, and a duplicate scene for testing

---
+++ PROC204/Assets/Scripts/UIScripts/SubtitlesUI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubtitlesUI : MonoBehaviour
+{
+    //Ensure that these two are the same length
+    public Sprite[] subtitleSprites;
+    public int[] subtitleLengths;
+
+    //Rest of variables
+    Canvas SubtitlesCanvas;
+    Image currentSubtitle;
+    public int currentSubtitleNumber = -1;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void ShowSubtitle(int durationOfSubtitles, Sprite SubtitleToDisplay)
+    {
+
+    }
+    public void UpdateImage()
+    {
+        currentSubtitleNumber = currentSubtitleNumber + 1;
+        currentSubtitle.overrideSprite = subtitleSprites[currentSubtitleNumber];
+        currentSubtitle.sprite = subtitleSprites[currentSubtitleNumber];
+    }
+    IEnumerator DisplaySubtitle(int SubtitleLength)
+    {
+        //First, enable the canvas
+        SubtitlesCanvas.enabled = true;
+        yield return new WaitForSeconds(SubtitleLength);
+        //Secondly, remove the canvas after waiting a certain amount of time.
+        SubtitlesCanvas.enabled = false;
+        yield return false;
+    }
+}
