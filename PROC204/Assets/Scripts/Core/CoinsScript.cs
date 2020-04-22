2b436ee018ced787434431c59b431d2d4b015c16
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2020-04-22 11:53:02 +0100

    Implementation of coins script

---
+++ PROC204/Assets/Scripts/Core/CoinsScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinsScript : MonoBehaviour
+{
+    public int coinsCollected = 0;
+    Text coinCounter;
+    void Start()
+    {
+        coinCounter = GameObject.Find("Coins Text").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter() ///Triggered when the player walks into a coin
+    {
+        if (this.gameObject.tag == "Coin")
+        {
+            coinsCollected = coinsCollected + 1; //Grant the player another coin
+            Destroy(this.gameObject); //Destroy the coin once it is completed
+            UpdateCounter(); //Update the coin counter
+        }
+    }
+
+    void UpdateCounter()
+    {
+        string display = "Coins: " + coinsCollected.ToString() + "/15";
+        coinCounter.text = display;
+        if (coinsCollected >= 15)
+        {
+            UnlockEndOfStage();
+        }
+
+    }
+
+    void UnlockEndOfStage()
+    {
+
+    }
+}
