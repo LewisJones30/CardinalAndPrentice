df7de36e9619736ef57f5c6bf7635bc6a7e45d32
ShemSkillman <43577362+ShemSkillman@users.noreply.github.com>
2020-05-06 21:12:17 +0100

    Added music
    Completed tasks:
    - Added music tracks to main menu, gameover, level 1 and level 2

---
+++ PROC204/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    [SerializeField] AudioClip[] musicTracks;
+    [SerializeField] AudioClip gameOverTrack;
+    [SerializeField] float delayBetweenTracks = 2f;
+
+    bool gameOverMusicPlaying = false;
+
+    Health cardinalHealth;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine(PlayMusic());
+
+        GameObject p1 = GameObject.FindGameObjectWithTag("Player 1");
+        cardinalHealth = p1.GetComponent<Health>();
+    }
+
+    private void Update()
+    {
+        if (cardinalHealth.IsDead  && !gameOverMusicPlaying)
+        {
+            gameOverMusicPlaying = true;
+            StopAllCoroutines();
+            audioSource.Stop();
+            audioSource.clip = gameOverTrack;
+            audioSource.Play();
+        }
+    }
+
+    IEnumerator PlayMusic()
+    {
+        int index = Random.Range(0, musicTracks.Length);
+
+        while (true)
+        {
+            audioSource.clip = musicTracks[index];
+            audioSource.Play();
+
+            yield return new WaitForSeconds(audioSource.clip.length + delayBetweenTracks);
+
+            index++;
+            if (index >= musicTracks.Length) index = 0;
+        }
+    }
+}
